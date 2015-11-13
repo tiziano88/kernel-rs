2f1a2deffa0af86e164c02cc09b545a18d6ba77d
Tiziano Santoro <tzn@google.com>
2015-11-13 19:24:49 +0000

    working!

--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -3,7 +3,7 @@ header_start:
   dd 0xe85250d6
   dd 0
   dd header_end - header_start
-  dd 0x10000000 - (0xe85250d6 + 0 + (header_end - header_start))
+  dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
   dw 0
   dw 0
